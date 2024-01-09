535ad86d486c676e0388a5e54cdc295103ea02d8
Yaner77 <kancur.yan77@mail.ru>
2024-01-09 15:02:50 +0300

    We wrote a program for the task in the file Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] LessThanThreeSign(string[] input, int n)
+{
+    string[] output = new string[GetArray(input, n)];
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= 0)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int GetArray(string[] input, int n)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] PrintArray()
+{
+    Console.WriteLine("Введите значение массива через пробел: ");
+    return Console.ReadLine().Split(" ");    
+}
+
+Console.Clear();
+string[] array = PrintArray();
+string[] result = LessThanThreeSign(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
